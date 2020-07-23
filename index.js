ace273864279fb000ce4f486c0bbd701f8e30c45
berellevy <berellevy@gmail.com>
2020-07-23 17:06:45 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 class Formatter {
-  //add static methods here
+    static capitalize(string) {
+        return string.replace(/(^\w)/, (match) => match.toUpperCase())
+    }
+
+    static sanitize(string) {
+      return string.replace(/[^A-Za-z0-9-' ]+/g, '')
+    }
+
+    static titleize(string) {
+        let exclude = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by','from'];
+        let newString = string.replace(/\b(\w+)/g, (word) => {
+          if (exclude.includes(word)) {
+            return word;
+          } else {
+            return this.capitalize(word);
+          }
+        })
+        return this.capitalize(newString)
+    }
+
+
 }
